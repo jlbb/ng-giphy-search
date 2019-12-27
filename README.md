dd22f9862aba2bed9433557cae57d7c017e30d8b
Jose Luis <joseluis.barreche@gmail.com>
2019-12-27 21:59:43 +0100

    Create search-bar component. Modify app component template. Refactor app component to scss file

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+### Aditional developer's notes
+
+For changing the dev server port you can check how to do it here: https://stackoverflow.com/a/51175153/1186541. Either adding e.g. `port: 8080` in the serve -> options of the project or run `ng serve --port 8080` using the cli.
+
+`FormsModule` is necessary to be added to use the `[(ngModel)]` two-way data bindings: https://angular.io/guide/template-syntax#import-formsmodule-to-use-ngmodel
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { SearchBarComponent } from "./search-bar/search-bar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SearchBarComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
